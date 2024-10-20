3c6bdc01b573bd4d2b1635d0385e43263b615cd0
Sergei Bykov <serega1988bykov@gmail.com>
2024-10-20 13:40:36 +0500

    introduce Templates

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -7,23 +7,12 @@ use App\Models\Bb;
 
 class BbsController extends Controller
 {
-    public function index(){
-    	$bbs = Bb::latest()->get();
-        $s = 'Объявления\r\n\r\n';
-        foreach ($bbs as $bb){
-            $s .= $bb->title . '\r\n';
-            $s .= $bb->price . ' руб.\r\n';
-            $s .= '\r\n';
-        }
-        return response($s)->header('Content-Type', 'text/plain');
+    public function index() {
+        $context = ['bbs' => Bb::latest() -> get()];
+        return view('index', $context);
     }
 
-    public function detail(Bb $bb) {
-        # $bb = Bb::find($bb);
-        $s = $bb->title . "\r\n\r\n";
-        $s .= $bb->content . "\r\n";
-        $s .= $bb->price . " руб.\r\n";
-        return response($s)->header('Content-Type', 'text/plain');
-        }
+    public function detail(Bb $bb){
+        return view('detail', ['bb' => $bb]);
+    }
 }
-?>
