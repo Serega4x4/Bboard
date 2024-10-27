9507dad8ebdd25538c8f745b94a10a977f507592
Serega4x4 <serega1988bykov@gmail.com>
2024-10-27 17:42:54 +0500

    validate of data

--- resources/views/bb-create.blade.php
+++ resources/views/bb-create.blade.php
@@ -6,16 +6,20 @@
     @csrf
     <div class="mb-3">
         <label for="txtTitle" class="form-label">Товар</label>
-        <input name="title" id="txtTitle" class="form-control">
+        <input name="title" id="txtTitle" 
+                class="form-control @error('title') is-invalid @enderror">
     </div>
     <div class="mb-3">
         <label for="txtContent" class="form-label">Описание</label>
-        <textarea name="content" id="txtContent" class="form-control" row="3"></textarea>
+        <textarea name="content" id="txtContent" 
+                    class="form-control @error('content') is-invalid @enderror" 
+                    row="3"></textarea>
     </div>
     <div class="mb-3">
         <label class="txtPrice" class="form-label">Цена</label>
-        <input name="price" id="txtPrice" class="form-control">
+        <input name="price" id="txtPrice" 
+                class="form-control @error('price') is-invalid @enderror">
     </div>
-    <input type="submit" class="btn btn-primary" value="Добавить">
+    <input type="submit" class="btn btn-primary" value="{{ old('title') }}">
 </form>
 @endsection('content')
--- resources/views/bb-edit.blade.php
+++ resources/views/bb-edit.blade.php
@@ -7,15 +7,21 @@
     @method('PATCH')
     <div class="mb-3">
         <label for="txtTitle" class="form-label">Товар</label>
-        <input name="title" id="txtTitle" class="form-control" value="{{ $bb->title }}">
+        <input name="title" id="txtTitle" 
+                class="form-control @error('title') is-invalid @enderror" 
+                value="{{ old('title', $bb->title) }}">
     </div>
     <div class="mb-3">
         <label for="txtContent" class="form-label">Описание</label>
-        <textarea name="content" id="txtContent" class="form-control" row="3">{{ $bb->content }}</textarea>
+        <textarea name="content" id="txtContent" 
+                    class="form-control @error('content') is-invalid @enderror" 
+                    row="3">{{ old('content', $bb->content) }}</textarea>
     </div>
     <div class="mb-3">
         <label class="txtPrice" class="form-label">Цена</label>
-        <input name="price" id="txtPrice" class="form-control" value="{{ $bb->price }}">
+        <input name="price" id="txtPrice" 
+                class="form-control @error('price') is-invalid @enderror" 
+                value="{{ old('price', $bb->price) }}">
     </div>
     <input type="submit" class="btn btn-primary" value="Сохранить">
 </form>
